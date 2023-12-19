47d80e1cb0b00ae4da79902005742fa04bd6f42c
Scott Schwab <scottschwab86@gmail.com>
2023-12-19 11:05:14 -0600

    All Pianists and All students routes fetch and display data from db

--- client/src/components/AllPianists/AllPianists.jsx
+++ client/src/components/AllPianists/AllPianists.jsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 function AllPianists() {
+
+  const [allPianists, setAllPianists] = useState([]);
+
+  useEffect(() => {
+    fetch('/api/pianists')
+      .then(r => r.json())
+      .then(pianistData => setAllPianists(pianistData))
+  }, [])
+
   return (
     <div>
-      All Pianists
+      <h1>All pianists</h1>
+      <ul>
+        {allPianists.map((pianist) => <li key={pianist.id}>{pianist.name}</li>)}
+      </ul>
     </div>
   )
 }
--- client/src/components/AllStudents/AllStudents.jsx
+++ client/src/components/AllStudents/AllStudents.jsx
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 function AllStudents() {
+
+  const [allStudents, setAllStudents] = useState([]);
+
+  useEffect(() => {
+    fetch('/api/students')
+      .then(r => r.json())
+      .then(studentData => setAllStudents(studentData))
+  }, [])
+
   return (
-    <div>All Students</div>
+    <div>
+      <h1>All Students</h1>
+      <ul>
+        {allStudents.map((student) => <li key={student.id}>{student.name}</li>)}
+      </ul>
+    </div>
   )
 }
 
