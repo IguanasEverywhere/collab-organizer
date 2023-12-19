8e7eb4ee0c1aaf4bda1f734c8432655554966d95
Scott Schwab <scottschwab86@gmail.com>
2023-12-19 11:49:19 -0600

    Establish and migrate table relationships + serialize rules

--- server/models.py
+++ server/models.py
@@ -8,20 +8,37 @@ from config import db
 class Coordinator(db.Model, SerializerMixin):
   __tablename__ = "Coordinators"
 
+  serialize_rules = ('-students.coordinator', '-pianists.coordinator, -events.coordinator',)
+
   id = db.Column(db.Integer, primary_key=True)
   username = db.Column(db.String)
   organization = db.Column(db.String)
 
+  students = db.relationship('Student', back_populates='coordinator')
+  pianists = db.relationship('Pianist', back_populates='coordinator')
+  events = db.relationship('Event', back_populates='coordinator')
+
 class Pianist(db.Model, SerializerMixin):
   __tablename__ = "Pianists"
+
+  serialize_rules = ('-students.pianists', '-events.pianist', '-coordinator.pianists',)
 
   id = db.Column(db.Integer, primary_key=True)
   name = db.Column(db.String)
   role = db.Column(db.String)
   email = db.Column(db.String)
 
+  coordinator_id = db.Column(db.Integer, db.ForeignKey('Coordinators.id'))
+
+  students = db.relationship('Student', back_populates='pianists')
+  events = db.relationship('Event', back_populates='pianist')
+
+  coordinator = db.relationship('Coordinator', back_populates='pianists')
+
 class Student(db.Model, SerializerMixin):
   __tablename__ = "Students"
+
+  serialize_rules = ('-events.student', '-pianists.students, -coordinator.students',)
 
   id = db.Column(db.Integer, primary_key=True)
 
@@ -30,8 +47,17 @@ class Student(db.Model, SerializerMixin):
   teacher = db.Column(db.String)
   email = db.Column(db.String)
 
+  coordinator_id = db.Column(db.Integer, db.ForeignKey('Coordinators.id'))
+
+  events = db.relationship('Event', back_populates='student')
+  pianists = db.relationship('Pianist', back_populates='students')
+
+  coordinator = db.relationship('Coordinator', back_populates='students')
+
 class Event(db.Model, SerializerMixin):
   __tablename__ = "Events"
+
+  serialize_rules = ('-student.events', '-pianist.events, -coordinator.events',)
 
   id = db.Column(db.Integer, primary_key=True)
 
@@ -39,4 +65,12 @@ class Event(db.Model, SerializerMixin):
   event_time = db.Column(db.DateTime)
   event_length = db.Column(db.Integer)
   location = db.Column(db.String)
+
+  student_id = db.Column(db.Integer, db.ForeignKey('Students.id'))
+  pianist_id = db.Column(db.Integer, db.ForeignKey('Pianists.id'))
+  coordinator_id = db.Column(db.Integer, db.ForeignKey('Coordinators.id'))
+
+  student = db.relationship('Student', back_populates='events')
+  pianist = db.relationship('Pianist', back_populates='events')
+  coordinator = db.relationship('Coordinator', back_populates='events')
 
