747eb99c51845b5bf62b84d96f4e229a7be46107
Scott Schwab <scottschwab86@gmail.com>
2023-12-19 09:40:49 -0600

    Add all table resources with get all methods

--- server/app.py
+++ server/app.py
@@ -9,17 +9,13 @@ from flask_restful import Resource
 # Local imports
 from config import app, db, api
 # Add your model imports
-from models import Coordinator, Pianist
+from models import Coordinator, Pianist, Student, Event
 
 
 # Views go here!
 @app.route('/')
 def index(id=0):
     return render_template("index.html")
-
-# @app.route('/')
-# def index():
-#     return '<h1>Project Server</h1>'
 
 
 class Coordinators(Resource):
@@ -30,10 +26,36 @@ class Coordinators(Resource):
         response = make_response(coordinator_dicts, 200)
         return response
 
+class Pianists(Resource):
+    def get(self):
+        all_pianists = Pianist.query.all()
+        pianist_dicts = [pianist.to_dict() for pianist in all_pianists]
+
+        response = make_response(pianist_dicts, 200)
+        return response
+
+
+class Students(Resource):
+    def get(self):
+        all_students = Student.query.all()
+        student_dicts = [student.to_dict() for student in all_students]
+
+        response = make_response(student_dicts, 200)
+        return response
+
+class Events(Resource):
+    def get(self):
+        all_events = Event.query.all()
+        event_dicts = [event.to_dict() for event in all_events]
+
+        response = make_response(event_dicts, 200)
+        return response
+
 
 api.add_resource(Coordinators, '/api/coordinator', endpoint='/api/coordinator')
-
-
+api.add_resource(Pianists, '/api/pianists', endpoint='/api/pianists')
+api.add_resource(Students, '/api/students', endpoint='/api/students')
+api.add_resource(Events, '/api/events', endpoint='/api/events')
 
 
 if __name__ == '__main__':
