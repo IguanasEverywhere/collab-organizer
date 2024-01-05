363062e7d5a6bd3e7b887e95f54dab09da225769
Scott Schwab <scottschwab86@gmail.com>
2024-01-05 15:38:47 -0600

    Add redux with counter placeholder

--- client/src/components/AllEvents/AllEvents.jsx
+++ client/src/components/AllEvents/AllEvents.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Counter } from '../../features/Counter';
 
 function AllEvents() {
   return (
     <div>
       All Events
+      <Counter />
     </div>
   )
 }
--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,14 @@ import App from "./components/App";
 import "./index.css";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from 'react-router-dom';
+import store from './store';
+import { Provider } from 'react-redux';
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>);
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>);
