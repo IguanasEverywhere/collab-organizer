d66b295872d72914120bf1e57690c9c8b6eb9ecc
Scott Schwab <scottschwab86@gmail.com>
2024-01-10 14:51:20 -0600

    Begin light mode styling of event detail

--- client/src/components/EventInfo/EventInfo.js
+++ client/src/components/EventInfo/EventInfo.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams, Link } from 'react-router-dom';
+import lightStyles from './EventInfoLight.module.css';
 
 function EventInfo() {
 
@@ -21,14 +22,15 @@ function EventInfo() {
   }
 
   return (
-    <div>
+    <div className={lightStyles.mainBody}>
+      <div className={lightStyles.eventInfo}>
+        <h2>Student: <Link to={`/students/${eventInfo.student_id}`}>{eventInfo.student.name}</Link></h2>
+        <h3>{eventInfo.event_type}</h3>
+        <h5>{eventInfo.location}</h5>
+        <h5>{eventInfo.event_length} minutes</h5>
+        <h5>Pianist: </h5><Link to={`/pianists/${eventInfo.pianist_id}`}>{eventInfo.pianist.name}</Link>
 
-      <Link to={`/students/${eventInfo.student.id}`}><h2>{eventInfo.student.name}</h2></Link>
-      <h3>{eventInfo.event_type}</h3>
-      <h5>{eventInfo.location}</h5>
-      <h5>{eventInfo.event_length} minutes</h5>
-      <h5>Pianist: </h5><Link to="/">{eventInfo.pianist.name}</Link>
-
+      </div>
     </div>
   )
 }
