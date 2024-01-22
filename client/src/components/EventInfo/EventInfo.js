9661f27dc51b633bebae26604f44576410255054
Scott Schwab <scottschwab86@gmail.com>
2024-01-22 16:22:11 -0600

    Continue dark mode style application

--- client/src/components/EventInfo/EventInfo.js
+++ client/src/components/EventInfo/EventInfo.js
@@ -45,16 +45,16 @@ function EventInfo() {
 
       {modalVisible ? <EditEventModal setModalVisible={setModalVisible} eventInfo={eventInfo} /> : null}
 
-      <div className={lightStyles.eventInfo}>
+      <div className={currentStyle.eventInfo}>
         <h2>Student: <Link to={`/students/${eventInfo.student_id}`}>{eventInfo.student.name}</Link>: {eventInfo.event_type}</h2>
         <h5>{new Date(eventInfo.event_time).toLocaleString()}</h5>
         <h5>{eventInfo.location}</h5>
         <h5>{eventInfo.event_length} minutes</h5>
         {/* <h5>Pianist: <Link to={`/pianists/${eventInfo.pianist_id}`}>{eventInfo.pianist.name}</Link></h5> */}
         {eventInfo.pianist_id ? <h5>Pianist: <Link to={`/pianists/${eventInfo.pianist_id}`}>{eventInfo.pianist.name}</Link></h5>: <p>unassigned</p>}
-        <div className={lightStyles.buttonsArea}>
-          <button className={lightStyles.eventBtns} onClick={handleEditClick}>Edit Event</button>
-          <button className={lightStyles.eventBtns} onClick={handleDeleteClick}>Delete Event</button>
+        <div className={currentStyle.buttonsArea}>
+          <button className={currentStyle.eventBtns} onClick={handleEditClick}>Edit Event</button>
+          <button className={currentStyle.eventBtns} onClick={handleDeleteClick}>Delete Event</button>
         </div>
 
       </div>
