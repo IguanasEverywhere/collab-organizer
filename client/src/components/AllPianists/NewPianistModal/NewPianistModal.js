7faaaf0526774ab354c0aada8bb145524f769612
Scott Schwab <scottschwab86@gmail.com>
2024-01-22 17:22:17 -0600

    Continue adding dark mode styling particulars

--- client/src/components/AllPianists/NewPianistModal/NewPianistModal.js
+++ client/src/components/AllPianists/NewPianistModal/NewPianistModal.js
@@ -1,13 +1,19 @@
 import React from 'react';
-import styles from './NewPianistModal.module.css';
+import lightStyles from './NewPianistModalLight.module.css';
+import darkStyles from './NewPianistModalDark.module.css';
 import Backdrop from '../../Backdrop/Backdrop';
 import * as yup from 'yup';
 import { useFormik } from 'formik';
 import { useHistory } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
 
 function NewPianistModal({ setModalVisible }) {
 
   const history = useHistory();
+
+  const viewMode = useSelector(state => state.viewMode.value)
+  const currentStyle = viewMode === "light" ? lightStyles : darkStyles
 
   const newPianistSchema = yup.object().shape({
     name: yup.string().required(),
@@ -38,11 +44,11 @@ function NewPianistModal({ setModalVisible }) {
   }
 
   return (
-    <div className={styles.modalContainer}>
+    <div className={currentStyle.modalContainer}>
       <Backdrop />
-      <div className={styles.modalBody}>
-        <div className={styles.closeBtnArea}>
-          <button onClick={handleCloseClick} className={styles.closeBtn}>
+      <div className={currentStyle.modalBody}>
+        <div className={currentStyle.closeBtnArea}>
+          <button onClick={handleCloseClick} className={currentStyle.closeBtn}>
             &nbsp;X&nbsp;
           </button>
         </div>
@@ -54,7 +60,7 @@ function NewPianistModal({ setModalVisible }) {
             name="name"
             value={pianistFormik.name}
             onChange={pianistFormik.handleChange}></input>
-            <small>{pianistFormik.errors.name}</small>
+          <small>{pianistFormik.errors.name}</small>
 
           <label htmlFor="role">Pianist Role</label>
           <select
@@ -73,10 +79,10 @@ function NewPianistModal({ setModalVisible }) {
             name="email"
             value={pianistFormik.email}
             onChange={pianistFormik.handleChange}></input>
-            <small>{pianistFormik.errors.email}</small>
+          <small>{pianistFormik.errors.email}</small>
 
 
-          <button className={styles.submitBtn} type="submit">Add New Pianist</button>
+          <button className={currentStyle.submitBtn} type="submit">Add New Pianist</button>
 
         </form>
 
